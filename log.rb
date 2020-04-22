3e07087d44a942c1f9563a2cd2d6bb7e0f7361fd
Javi <highavoc@yahoo.es>
2020-04-23 00:41:17 +0200

    Add log class

---
+++ log.rb
@@ -0,0 +1,80 @@
+#!/cygdrive/c/Ruby24-x64/bin/ruby
+#
+# @ Javier Felipe Toribio 2020-04-22
+#
+# Manejador de trazas
+#
+
+class Log
+
+  # Nivel del log
+  CRITICO, ERROR, ALERTA, INFO = 1, 2, 3, 4
+
+  attr_accessor :nivel
+
+  def initialize(fichero = nil, nivel = nil)
+
+    @nivel = nivel || INFO
+
+    nombre_fichero = fichero || "traductor"
+
+    nombre_fichero = "#{prefijo_tiempo_fichero}_#{nombre_fichero}.log"
+
+    begin
+
+       @log = open(nombre_fichero,"a+")
+       @log.sync = true
+       @abierto = true
+
+    rescue StandardError => e
+
+       puts e.message
+       puts e.backtrace.inspect
+
+    end
+
+  end
+
+  def info(mensaje)    trazar(INFO, "INFO    : " << mensaje) end
+  def warn(mensaje)    trazar(INFO, "ALERTA  : " << mensaje) end
+  def error(mensaje)   trazar(INFO, "ERROR   : " << mensaje) end
+  def critico(mensaje) trazar(INFO, "CRITICO : " << mensaje) end
+
+  def cerrar
+
+    @log.close if @abierto
+    @log = nil
+
+  end
+
+private
+
+  def trazar(nivel, mensaje)
+
+     if @log && nivel <= @nivel
+
+        @log << prefijo_tiempo_mensaje() << " " << mensaje << "\n"
+
+     end
+
+  end
+
+  def prefijo_tiempo_fichero
+
+     formato = "%Y-%m-%d_%H_%M_%S"
+
+     Time.now.strftime(formato)
+
+  end
+
+  # Fecha, hora con milisegundos
+
+  def prefijo_tiempo_mensaje
+
+     formato = "[%Y-%m-%d %H:%M:%S.%L]"
+
+     Time.now.strftime(formato)
+
+  end
+
+end
---
+++ test_log.rb
@@ -0,0 +1,28 @@
+#!/cygdrive/c/Ruby24-x64/bin/ruby
+#
+# @ Javier Felipe Toribio 2020-04-22
+#
+# Test Manejador de trazas
+#
+
+require_relative "log.rb"
+
+log = Log.new("test",Log::INFO)
+
+log.info("Mensaje informativo")
+
+log.warn("Poca memoria")
+
+log.error("No se pudo abrir el fichero de configuracion")
+
+log.critico("La aplicacion se ha quedado sin memoria")
+
+log.cerrar
+
+log = Log.new
+
+log.error("Disco duro al 95%")
+
+log.cerrar
+
+
